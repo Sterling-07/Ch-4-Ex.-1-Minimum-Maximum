ced5bc4fb435683d4bf98423a3007f5880da8cd7
sterl <sterlingrodgers2007@gmail.com>
2025-09-29 10:47:31 -0400

    Add project files.

---
+++ Ch 4 Ex. 1 Minimum-Maximum.cpp
@@ -0,0 +1,34 @@
+//File Name: Chapter 4 - Minimum/Maximum
+//Programmer: Philip Rodgers
+//Date: September 2025
+//Requirements: Create a program that allows the user to enter two numbers and can determine which one is larger and which one is smaller. 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//Create necessary variables
+	int a, b;
+	
+	//Prompt the user to enter the first and second number
+	cout << "Enter your first number: \n";
+	cin >> a;
+	cout << "Enter your second number: \n";
+	cin >> b;
+
+	//Determine whether a is larger, smaller, or equal to b and display the result
+	if (a > b)
+	{
+		cout << a << " is larger than " << b;
+	}
+	if (a < b)
+	{
+		cout << b << " is larger than " << a;
+	}
+	if (a == b)
+	{
+		cout << a << " is equal to " << b;
+	}
+}
+
